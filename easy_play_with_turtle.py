6dc35880392e739f8c4ded2042d6815d713ed375
anonymoustaco <64505411+anonymoustaco@users.noreply.github.com>
2020-05-19 16:23:51 -0500

    Update easy_play_with_turtle.py

--- easy_play_with_turtle.py
+++ easy_play_with_turtle.py
@@ -1,7 +1,9 @@
-def how_to_use():
+i = 100
+def how_to_use():    
     import turtle
     print("for shapes enter polygon() the number of sides a coma")
     print("then the a space lastly the size. ")
+    print('circle(size) creates a circle.')
 
 def polygon(length, sides):
     import turtle
@@ -11,5 +13,10 @@ def polygon(length, sides):
         print("side done")
     print("complete!")
 
+def circle(size):
+    import turtle
+    for size in range (size + 30):
+        turtle.forward(size)
+        turtle.right(size)        
 
 
