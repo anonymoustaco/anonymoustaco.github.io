0d18dbf077a933b96b9f9ba98a79515a5b55ff13
anonymoustaco <64505411+anonymoustaco@users.noreply.github.com>
2020-05-19 11:50:56 -0500

    Update easy_play_with_turtle.py

--- easy_play_with_turtle.py
+++ easy_play_with_turtle.py
@@ -1,1 +1,15 @@
+def how_to_use():
+    import turtle
+    print("for shapes enter polygon() the number of sides a coma")
+    print("then the a space lastly the size. ")
+
+def polygon(length, sides):
+    import turtle
+    for side in range(sides):
+        turtle.forward(length)
+        turtle.right(360/sides)
+        print("side done")
+    print("complete!")
+
+
 
